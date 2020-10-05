3f3d7b44c8a7f5c65e84af43d75298e38c021ade
KatherineHarley <55381694+KatherineHarley@users.noreply.github.com>
2020-10-05 13:41:28 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,19 +3,14 @@ var btns = document.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  var current = document.getElementsByClassName("active-sect");
-  current[0].className = current[0].className.replace(" active-sect", " inactive-sect");
-  this.className += " active";
-  console.log(this.text)
-  if (this.text == "m&s"){
-      var use = "mns";
-  }
-  else{
-      var use = this.text;
-  }
-  var toChange = document.getElementById(use).getElementsByClassName("content")[0]
-  toChange.className = toChange.className.replace(" inactive-sect", " active-sect");
-  console.log(document.getElementById(use).getElementsByClassName("content")[0].className)
+  current[0].className = current[0].className.replace(" active", " nonactive");
+  
+  // current[0].className = current[0].className.replace(" active-sect", " inactive-sect");
+  this.className = this.className.replace(" nonactive", " active")
+  // console.log(this.href.split("#").pop())
+  // var use = this.href.split("#").pop()
+  // var toChange = document.getElementById(use).getElementsByClassName("content")[0]
+  // toChange.className = toChange.className.replace(" inactive-sect", " active-sect");
+  // console.log(document.getElementById(use).getElementsByClassName("content")[0].className)
 });
 }
